8b521c103a43befc2069d7d798d70811a83b50a9
Michael Way <mway@Michaels-Mac-mini.local>
2023-08-23 22:10:11 -0400

    setup react router

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 const root = createRoot(document.getElementById('root') as Element);
-root.render(<App />);
+root.render(
+  // TO-DO: remove strict mode for production
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
