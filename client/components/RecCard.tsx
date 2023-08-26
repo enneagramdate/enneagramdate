8358668a3cd89e2b0f1398071d112130c48d39fe
Michael Way <mway@Michaels-Mac-mini.local>
2023-08-26 13:11:46 -0400

    confirmed neither clearSwipes nor clearRecs is working as intended

--- client/components/RecCard.tsx
+++ client/components/RecCard.tsx
@@ -10,12 +10,10 @@ const RecCard = (user: User) => {
   // import recs state so handleSwipe can update it
   const recs: User[] = recsStore.use.recs();
   const removeOneRec = recsStore.use.removeOneRec();
+  const clearRecs = recsStore.use.clearRecs();
   const swipes: SwipeCache = userStore.use.swipes();
   const updateSwipes = userStore.use.updateSwipes();
   const clearSwipes = userStore.use.clearSwipes();
-
-  console.log('HERE ARE THE STORED SWIPES FOR BATCH INSERT', swipes);
-  console.log('HERE IS THE  NUMBER OF SWIPES STORED', swipes.size);
 
   // declare a function to handleSwipe
   const handleSwipe = (swipe: Swipe, swipedUserId: UserId) => {
@@ -23,44 +21,28 @@ const RecCard = (user: User) => {
     const outcome = swipe === 'like' ? 'like' : 'dislike';
     // * ping DB to update relationship accordingly -- better option would be to update the DB in batches, rather than on every swipe to increase performance and UX
     // ! what number is best here for batch size?
-    if (swipes.size >= 5) {
+    if (swipes.size >= 2) {
       // first convert swipes state (a Map) into an object
       const swipesObj = Object.fromEntries(swipes);
       // then convert to JSON
       const swipesBatch = JSON.stringify(swipesObj);
+      console.log(swipesObj);
       // ? then ping the DB with the batch -- will need to talk to Upasana about if it's feasible to do it this way in Neo4j
       // after the response, clear the swipes state
       clearSwipes();
+      clearRecs();
     }
     // * if it isn't time to ping the DB yet, we just update swipes state
     const updatedSwipes = new Map(swipes);
     updatedSwipes.set(swipedUserId!, swipe);
     updateSwipes(updatedSwipes);
+    // console.log('HERE ARE THE STORED SWIPES FOR BATCH INSERT', swipes);
+    // console.log('HERE IS THE  NUMBER OF SWIPES STORED', swipes.size);
     // * and then update the recs state to remove the person we just swiped
     const updatedState = [...recs];
     updatedState.pop();
     removeOneRec(updatedState);
   };
-
-  // // ! probably need to store the swipe cache in user state so it is globally persisted, put this on userStore
-  // const handleSwipeCache = () => {
-  //   const cache: SwipeCache = new Map();
-  //   return async (swipe: Swipe, swipedUserId: RecId) => {
-  //     // determine which swipe this is
-  //     const outcome = swipe === 'like' ? 'like' : 'dislike';
-  //     // store that outcome in the cache
-  //     cache.set(swipedUserId, swipe);
-  //     // if the cache is a certain size, ping the DB with the batched updated
-  //     // first convert map to an object
-  //     i;
-  //     const swipes = Object.fromEntries(cache);
-
-  //     // update the recs state to remove the just-swiped person
-  //     const updatedState = [...recs];
-  //     updatedState.pop();
-  //     removeOneRec(updatedState);
-  //   };
-  // };
 
   return (
     <div className="card w-96 bg-primary text-primary-content">
