7603d5a3d2d88352c4f38c160d4b343fa66b682f
Michael Way <mway@Michaels-Mac-mini.local>
2023-08-27 12:42:03 -0400

    cleaned up color workaround

--- client/components/EnneagramBadge.tsx
+++ client/components/EnneagramBadge.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { EnneagramType } from '../types';
-// ! this is weird, but given the way tailwind compiles, the dynamic color classes won't load properly unless this is imported
-// ! see: https://www.codeconcisely.com/posts/tailwind-css-dynamic-class/
-import { colorWorkaround } from '../data/utils';
 
 const EnneagramBadge = ({
   enneagramType,
@@ -19,15 +16,3 @@ const EnneagramBadge = ({
 };
 
 export default EnneagramBadge;
-
-/* 
-      9: '#EC50B6',
-      8: '#CF0010',
-      7: '#F8A400',
-      6: '#FBD504',
-      5: '#78CD34',
-      4: '#32A75F',
-      3: '#329F9D',
-      2: '#21669E',
-      1: '#2826A7',
-*/
--- client/data/utils.ts
+++ client/data/utils.ts
@@ -4,5 +4,7 @@ export const mapToJSON = (map: Map<any, any>) => {
   return JSON.stringify(Object.fromEntries(map));
 };
 
-export const colorWorkaround =
-  'color can be bg-9 bg-8 bg-7 bg-6 bg-5 bg-4 bg-3 bg-2 or bg-1';
+// ! Given the way tailwind compiles dynamic classes, the dynamic color classes won't load properly the
+// ! post-resolution class name exists somewhere in the codebase, so it is being put here as a global reference
+// ! see: https://www.codeconcisely.com/posts/tailwind-css-dynamic-class/
+export const colorWorkaround = 'bg-9 bg-8 bg-7 bg-6 bg-5 bg-4 bg-3 bg-2 bg-1';
