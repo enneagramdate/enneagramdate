1ae87ff4950308241502b3c244d690055874d036
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-08-29 21:14:29 -0500

    all signup field added

--- client/Signup.tsx
+++ client/Signup.tsx
@@ -1,16 +1,17 @@
 import React, { KeyboardEventHandler, useState } from 'react';
-import { google } from 'google-maps';
+import { Loader } from '@googlemaps/js-api-loader';
 import useUserStore, { UserState } from './userStore';
 import isEmail from 'validator/lib/isEmail';
+import isPostalCode from 'validator/lib/isPostalCode';
 import { useNavigate } from 'react-router-dom';
 
 interface Info {
   email: string;
   password: string;
   fullName: string;
-  enneagramType: number;
+  enneagramType: string;
   birthday: string;
-  seekAgeRange: number[] | null;
+  seekAgeRange: number[];
   gender: string;
   seekGender: string;
   seekRelationship: string;
@@ -18,13 +19,27 @@ interface Info {
   seekRadius: number;
 }
 
+type infoKey =
+  | 'email'
+  | 'password'
+  | 'fullName'
+  | 'enneagramType'
+  | 'birthday'
+  | 'seekAgeRangeLow'
+  | 'seekAgeRangeHigh'
+  | 'gender'
+  | 'seekGender'
+  | 'seekRelationship'
+  | 'position'
+  | 'seekRadius';
+
 const defaultInfo: Info = {
   email: '',
   password: '',
   fullName: '',
-  enneagramType: 0,
+  enneagramType: '',
   birthday: '',
-  seekAgeRange: null,
+  seekAgeRange: [17, 16],
   gender: '',
   seekGender: '',
   seekRelationship: '',
@@ -32,51 +47,90 @@ const defaultInfo: Info = {
   seekRadius: 0,
 };
 
+const loader = new Loader({
+  apiKey: 'AIzaSyBro2kUXbOjXXxiQqn7bhx1Udcf5Nowx4c',
+  version: 'weekly',
+});
+
 const Signup = () => {
   const user: UserState = useUserStore((state) => state);
   const navigate = useNavigate();
-  const geocoder = new google.maps.Geocoder();
+
+  const [date, setDate] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+  });
 
   const [info, updateInfo] = useState(defaultInfo);
 
   const zipToPos = (zip: string) => {
-    geocoder.geocode({ address: zip }, function (results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        const centroid = results[0].geometry.location;
-        const curInfo = { ...info };
-        curInfo.position = centroid;
-        updateInfo(curInfo);
-      } else {
-        const curErr = { ...errors };
-        curErr.position = status;
-        updateErrors(curErr);
-      }
+    loader.importLibrary('geocoding').then((lib) => {
+      const geocoder = new lib.Geocoder();
+      geocoder.geocode({ address: zip }, function (results, status) {
+        if (results && results.length) {
+          const centroid = results[0].geometry.location;
+          const curInfo = { ...info };
+          curInfo.position = centroid;
+          updateInfo(curInfo);
+        } else {
+          const curErr = { ...errors };
+          curErr.position = status;
+          updateErrors(curErr);
+        }
+      });
     });
   };
 
   const [errors, updateErrors] = useState({
     email: false,
     go: true,
+    zip: false,
     position: '',
+    radius: false,
+    lowAge: false,
+    highAge: false,
     alert: '',
   });
 
-  const inputHandler = (text: string, type: 'email' | 'password') => {
+  const inputHandler = (text: string, type: infoKey) => {
     errorHandler(text, type);
     const curInfo = { ...info };
-    curInfo[type] = text;
+    if (type === 'position') {
+      if (!errors.zip) zipToPos(text);
+      return;
+    } else if (type === 'seekAgeRangeLow') {
+      if (!errors.lowAge) curInfo.seekAgeRange[0] = Number(text);
+    } else if (type === 'seekAgeRangeHigh') {
+      if (!errors.highAge) curInfo.seekAgeRange[1] = Number(text);
+    } else if (type === 'seekRadius') {
+      if (!errors.radius) curInfo.seekRadius = Number(text);
+    } else curInfo[type] = text;
     updateInfo(curInfo);
   };
 
-  const errorHandler = (text: string, type: 'email' | 'password') => {
+  const errorHandler = (text: string, type: infoKey) => {
     const curErr = { ...errors };
     curErr.alert = '';
     if (type === 'email') {
       curErr.email = !isEmail(text);
+    } else if (type === 'position') {
+      curErr.zip = !isPostalCode(text, 'US');
+    } else if (type === 'seekAgeRangeLow') {
+      curErr.lowAge =
+        !Number(text) ||
+        Number(text) < 18 ||
+        Number(text) > info.seekAgeRange[1];
+    } else if (type === 'seekAgeRangeHigh') {
+      curErr.highAge = !Number(text) || Number(text) < info.seekAgeRange[0];
+    } else if (type === 'seekRadius') {
+      curErr.radius = Number(text) < 1;
     }
     curErr.go = false;
     for (const [key, val] of Object.entries(info)) {
-      if (val === '') {
+      if (
+        !val ||
+        (key === 'seekAgeRange' && (val[0] === 17 || val[1] === 16))
+      ) {
         curErr.go = true;
         break;
       }
@@ -128,6 +182,121 @@ const Signup = () => {
             className="input input-bordered"
             onChange={(event) => inputHandler(event.target.value, 'password')}
             onKeyDown={keyDownHandler}
+          />
+        </label>
+        <label className="input-group">
+          <span>Name</span>
+          <input
+            type="text"
+            placeholder="Steve Harvey"
+            className={`input input-bordered`}
+            onChange={(event) => inputHandler(event.target.value, 'fullName')}
+          />
+        </label>
+        <label className="input-group">
+          <span>Enneagram Type</span>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={(event) =>
+              inputHandler(event.target.value, 'enneagramType')
+            }
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+          </select>
+        </label>
+        <label className="input-group">
+          <span>Birthday</span>
+          <input
+            type="date"
+            className={`input input-bordered`}
+            onChange={(event) => inputHandler(event.target.value, 'birthday')}
+          />
+        </label>
+        <label className="input-group">
+          <span>Zip Code</span>
+          <input
+            type="text"
+            className={`input input-bordered ${
+              errors.zip ? 'input-error' : ''
+            }`}
+            onChange={(event) => inputHandler(event.target.value, 'position')}
+          />
+        </label>
+        <label className="input-group">
+          <span>Gender</span>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={(event) => inputHandler(event.target.value, 'gender')}
+          >
+            <option>Female</option>
+            <option>Male</option>
+            <option>Non-binary</option>
+          </select>
+        </label>
+        <label className="input-group">
+          <span>Seeking</span>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={(event) => inputHandler(event.target.value, 'seekGender')}
+          >
+            <option>Female</option>
+            <option>Male</option>
+            <option>Non-binary</option>
+          </select>
+        </label>
+        <label className="input-group">
+          <span>Seeking</span>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            onChange={(event) =>
+              inputHandler(event.target.value, 'seekRelationship')
+            }
+          >
+            <option>Serious</option>
+            <option>Casual</option>
+          </select>
+        </label>
+        <label className="input-group">
+          <span>Within</span>
+          <input
+            type="text"
+            className={`input input-bordered ${
+              errors.radius ? 'input-error' : ''
+            }`}
+            onChange={(event) => inputHandler(event.target.value, 'seekRadius')}
+          />
+          <span>Miles</span>
+        </label>
+        <label className="input-group">
+          <span>Between</span>
+          <input
+            type="text"
+            placeholder="18"
+            className={`input input-bordered ${
+              errors.lowAge ? 'input-error' : ''
+            }`}
+            onChange={(event) =>
+              inputHandler(event.target.value, 'seekAgeRangeLow')
+            }
+          />
+          <span>and</span>
+          <input
+            type="text"
+            placeholder="118"
+            className={`input input-bordered ${
+              errors.highAge ? 'input-error' : ''
+            }`}
+            onChange={(event) =>
+              inputHandler(event.target.value, 'seekAgeRangeHigh')
+            }
           />
         </label>
         <button className="btn" disabled={errors.go} onClick={sender}>
